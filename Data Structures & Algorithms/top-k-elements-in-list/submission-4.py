974721349137cbef2d6e46763dd0f91c54292fb2
Landeri Srujan <66351075+srujan-landeri@users.noreply.github.com>
2026-08-04 20:31:54 +0530

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.py
@@ -0,0 +1,26 @@
+from queue import PriorityQueue
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        counter = dict()
+        for num in nums:
+            counter[num] = counter.get(num, 0) + 1
+        
+        freq = [0] * (len(nums) + 1)
+        for key,v in counter.items():
+            if freq[v] == 0:
+                freq[v] = []
+            freq[v].append(key)
+
+        # print(counter)
+        # print(freq)
+        i = len(freq) - 1
+        return_list = []
+        while k != 0 and i >= 0:
+            if freq[i] != 0:
+                return_list.extend(freq[i])
+                k -= len(freq[i])
+            i -= 1
+
+        return return_list
